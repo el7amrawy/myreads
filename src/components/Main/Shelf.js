cbe96a9e89b90a8a46cc0826faeb75ce694ae14d
Aly <el7amrawy@outlook.com>
2022-10-10 09:35:39 +0200

    Shelf inner satate removed

--- src/components/Main/Shelf.js
+++ src/components/Main/Shelf.js
@@ -1,16 +1,6 @@
-import { useState } from "react";
 import Book from "./Book";
 
 const Shelf = ({ name, filter, books }) => {
-  // console.log(books.books);
-  // const [shelfBooks, ,] = useState(
-  //   books.books.filter((book) => book.shelf === filter)
-  // );
-
-  // const bookElems = shelfBooks.map((book) => (
-  //   <Book key={book.id} book={book} books={books} />
-  // ));
-  //   console.log(name, books);
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{name}</h2>

9f296d22ec5b99ff3700094436cf860b1232f003
Aly <el7amrawy@outlook.com>
2022-10-10 09:36:10 +0200

    Shelf inner satate removed

--- src/components/Main/BookShelfChanger.js
+++ src/components/Main/BookShelfChanger.js
@@ -13,23 +13,20 @@ const BookShelfChanger = ({ book, books }) => {
         const newBooks = await getAll();
         books.setBooks(newBooks);
         localStorage.setItem("books", JSON.stringify(newBooks));
-        // console.log(books.books);
       })();
     }
   }, [shelf]);
-  // console.log(book.title, books);
+
   return (
     <div className="book-shelf-changer">
       <select
         onChange={(ev) => {
-          // console.log(ev.target.value);
           setShelf(ev.target.value);
         }}
       >
         <option value={shelf} disabled>
           Move to...
         </option>
-        {/* <option value="none">None</option> */}
         <option value="none">None</option>
         <option value="wantToRead">Want to Read</option>
         <option value="read">Read</option>
